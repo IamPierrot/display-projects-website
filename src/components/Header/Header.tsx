b1154fe1a9dc4a51662f90705975e6364886d1a6
KrySoSad <duythuc070205@gmail.com>
2024-01-16 10:34:59 +0700

    change header tag to nav tag, add active link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,30 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const headers = (
-    <>
-      <div className="group">
-        <li className="group-hover:text-[#A67E4E]">
-          <a href="/">HOME</a>
-        </li>
-      </div>
-      <div className="group">
-        <li className="group-hover:text-[#A67E4E]">
-          <a href="/pages">PAGE</a>
-        </li>
-      </div>
-      <div className="group">
-        <li className="group-hover:text-[#A67E4E]">
-          <a href="/api">API</a>
-        </li>
-      </div>
-      <div className="group">
-        <li className="group-hover:text-[#A67E4E]">
-          <a href="/team">TEAM</a>
-        </li>
-      </div>
-    </>
-  );
-  return (
-    <header className="fixed flex w-full flex-row items-center justify-between bg-[--primary-header-color] p-4 font-bold text-black shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-      <div className="cursor-pointer">
-        <h1 className="text-[#fff]">Logo Vjp Pro</h1>
-      </div>
-      <div className="pr-15 cursor-pointer space-x-3">
-        <ul className="flex flex-row items-center justify-between space-x-8 p-1 font-[Poppins] text-[#fff]">
-          {headers}
+  const [active, setActive] = useState("HOME");
 
-          <li className="relative shadow-black hover:shadow-lg">
+  let items = [
+    { name: "HOME", link: "/"},
+    { name: "PAGE", link: "/page"},
+    { name: "API", link: "/api"},
+    { name: "TEAM", link: "/team"},
+  ]
+  return (
+    <nav className="sticky top-0 z-50 flex w-full h-[60px] flex-row items-center justify-between bg-[--primary-header-color] p-4 font-bold text-white shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+      <div className="ml-10 cursor-pointer">
+        <Link to="/">
+          <img 
+            src="../../src/assets/logo.jpg" 
+            alt="logo image" 
+            className="w-full h-10 rounded-[50%]"
+            onClick={() => setActive("HOME")}
+          />
+        </Link>
+      </div> 
+      <div className="pr-15 cursor-pointer space-x-3 mr-12">
+        {/* <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
@@ -50,14 +39,23 @@ export const Header = () => {
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
               />
             </svg>
-          </li>
+        </div> */}
+        <ul className="flex justify-between items-center gap-8">
+          { items.map(i => (
+            <li 
+              className={`hover:text-[#5facf9] transition duration-200 ${active === i.name ? 'text-[#5facf9]' : 'text-[#fff]'}`}
+              onClick={() => setActive(i.name)}
+            >
+              <Link to={i.link} >{i.name}</Link>
+            </li>
+          ))}
         </ul>
-        {showMenu && (
+        {/* {showMenu && (
           <ul className="absolute w-full flex-col bg-[--primary-header-color] pt-2">
-            {headers}
+            
           </ul>
-        )}
+        )} */}
       </div>
-    </header>
+    </nav>
   );
 };
