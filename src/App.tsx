22132319ad38cad404cb4523b7be2366322d584d
KrySoSad <duythuc070205@gmail.com>
2024-01-16 10:33:32 +0700

    pass properties css from body to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import { Body } from "./components/Body/Body";
 import { EventScroll } from "./utils/eventScroll";
 
 function App() {
   return (
-    <div className="h-full w-full bg-white">
-      <EventScroll />
-      <Header />
-      <Body />
-      <Footer />
+    <div className="flex flex-col min-h-screen w-full bg-gradient-to-tr from-cyan-500 to-blue-900">
+      <Router>
+        <EventScroll />
+        <Header />
+        <Navigation/>
+        <Footer />
+      </Router>
     </div>
   );
 }
