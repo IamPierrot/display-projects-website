379899565f20b828f49c63377de97e87f08b23b2
IamPierrot <phatnguyentan836@gmail.com>
2024-01-19 21:13:52 +0700

    fix bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,48 +3,54 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [active, setActive] = useState("HOME"); 
+  const [active, setActive] = useState("HOME");
   const [styleStickMenu, setStyleStickMenu] = useState("");
   const [styleLink, setStyleLink] = useState("");
 
-  let items = [
-    { name: "HOME", link: "/"},
-    { name: "PAGE", link: "/page"},
-    { name: "API", link: "/api"},
-    { name: "TEAM", link: "/team"},
-  ]
+  const items = [
+    { name: "HOME", link: "/" },
+    { name: "PAGE", link: "/page" },
+    { name: "API", link: "/api" },
+    { name: "TEAM", link: "/team" },
+  ];
   useEffect(() => {
     const handleStickMenu = () => {
-      if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-        setStyleStickMenu("sticky z-50 bg-[--primary-header1-color] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] transition-all ease-in-out duration-500");
+      if (
+        document.body.scrollTop >= 200 ||
+        document.documentElement.scrollTop >= 200
+      ) {
+        setStyleStickMenu(
+          "sticky z-50 bg-[--primary-header-color] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] transition-all ease-in-out duration-500",
+        );
         setStyleLink("transition-all ease-in-out duration-500");
-      }
-      else {
+      } else {
         setStyleStickMenu("");
         setStyleLink("");
       }
-    }
+    };
 
     window.addEventListener("scroll", handleStickMenu);
 
     return () => {
       window.removeEventListener("scroll", handleStickMenu);
     };
-  }, [])
+  }, []);
 
   return (
-    <nav className={`top-0 flex w-full h-[60px] flex-row items-center justify-between p-4 font-bold font-mono text-white ${styleStickMenu}`}>
+    <nav
+      className={`top-0 flex h-[60px] w-full flex-row items-center justify-between p-4 font-mono font-bold text-white ${styleStickMenu}`}
+    >
       <div className="ml-10 cursor-pointer">
         <Link to="/">
-          <img 
-            src="../../src/assets/logo.jpg" 
-            alt="logo image" 
-            className="w-full h-10 rounded-[50%]"
+          <img
+            src="../../src/assets/logo.jpg"
+            alt="logo image"
+            className="h-10 w-full rounded-[50%]"
             onClick={() => setActive("HOME")}
           />
         </Link>
-      </div> 
-      <div className="pr-15 cursor-pointer space-x-3 mr-12">
+      </div>
+      <div className="pr-15 mr-12 cursor-pointer space-x-3">
         {/* <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -61,14 +67,16 @@ export const Header = () => {
               />
             </svg>
         </div> */}
-        <ul className="flex justify-between items-center gap-8">
-          { items.map((i, key) => (
-            <li 
+        <ul className="flex items-center justify-between gap-8">
+          {items.map((i, key) => (
+            <li
               key={key}
-              className={`hover:text-[#5facf9] transition duration-200 ${active === i.name ? 'text-[#5facf9]' : 'text-[#fff]'} ${styleLink}`}
+              className={`transition duration-200 hover:text-[#5facf9] ${
+                active === i.name ? "text-[#5facf9]" : "text-[#fff]"
+              } ${styleLink}`}
               onClick={() => setActive(i.name)}
             >
-              <Link to={i.link} >{i.name}</Link>
+              <Link to={i.link}>{i.name}</Link>
             </li>
           ))}
         </ul>
